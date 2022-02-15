da35b122ba089ce63d455e6f7ea606483619a177
PRESSO <caff1nepill@gmail.com>
2022-02-15 14:23:34 +0900

    First Commit

---
+++ server/api/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from .models import *
+from bs4 import BeautifulSoup as bs
+import sqlite3
+import os
+import subprocess
+from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
+import requests
+import json
+import time
+
+class ClassAPI(generics.GenericAPIView):
+    
+    def post(self, request, *args, **kwargs):
+        return Response(
+            {
+                "success": "true",
+            }
+        )
