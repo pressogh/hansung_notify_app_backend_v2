c3c60b39cf29fc0e6995cd964f52c7c08e0792cc
PRESSO <caff1nepill@gmail.com>
2022-02-15 14:38:23 +0900

    Chango Database to MySQL

--- server/server/settings.py
+++ server/server/settings.py
@@ -88,12 +88,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = get_secret("DATABASES")
 
 
 # Password validation
