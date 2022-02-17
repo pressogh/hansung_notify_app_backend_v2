ff423bcda83275594cdc4c98d951fd295819d62d
PRESSO <user1205@naver.com>
2022-02-17 12:36:22 +0900

    Jenkins TEST4

--- server/api/views.py
+++ server/api/views.py
@@ -14,7 +14,7 @@ import json
 import time
 
 class ClassAPI(generics.GenericAPIView):
-    # Jenkins TEST3
+    # Jenkins TEST4
     def post(self, request, *args, **kwargs):
         return Response(
             {
